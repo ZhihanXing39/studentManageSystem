79f38e8242f48c25179945d48111ae993a6ce88e
ZhihanXing39 <dreamcoldxing@qq.com>
2024-02-23 10:26:55 +0800

    整合了重复的CSS

--- student/addStu.php
+++ student/addStu.php
@@ -5,23 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>添加学生</title>
     <script src="../script/addStu.js" async></script>
-    <style>
-        *{
-            padding: 0;
-            margin: 0;
-            box-sizing: border-box;
-        }
-        .container{
-            width: 80rem;
-            height: 40rem;
-            border: .1rem solid;
-            background-color: #3399ff;
-            /* 布局信息 */
-            display: none;
-            justify-content: center;
-            align-items: center;
-        }
-    </style>
+    <link rel="stylesheet" href="../style/add.css">
 </head>
 <body>
     <button id="addButton">添加学生</button>
