16b4e2a6c8f56e85554be5d0328f643222095651
ZhihanXing39 <dreamcoldxing@qq.com>
2024-02-28 20:22:59 +0800

    修复了session_start重复启动的bug

--- utils/loginStatus.php
+++ utils/loginStatus.php
@@ -1,6 +1,8 @@
 <?php
     //这是验证用户是否登录的php
-    session_start();
+    if(session_id() == ""){
+        session_start();
+    }
     //用户未登录则强制返回登录页面
     if(!isset($_SESSION['username'])){
         header("Location:/login.php");
